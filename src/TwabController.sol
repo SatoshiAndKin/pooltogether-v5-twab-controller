32f5c89ae7781049deff07f44a72be4c8bdcba83
Bryan Stitt <bryan@stitthappens.com>
2024-12-08 00:47:05 -0800

    less strict version

--- src/TwabController.sol
+++ src/TwabController.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 import { TwabLib } from "./libraries/TwabLib.sol";
 import { ObservationLib } from "./libraries/ObservationLib.sol";
